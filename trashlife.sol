7f9574cfd0f66b4a5861b2bec6e83da61788f8ca
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-09 17:43:54 +0100

    Update TrashLife Contract (Version 1.0)

--- trashlife.sol
+++ trashlife.sol
@@ -4,11 +4,6 @@ import "citizen.sol";
 
 contract TrashLife is Agents {
     
-    modifier onlyMunicipality() {
-        require(municipalities[msg.sender].active == true, "Must have the municipality permission!");
-        _;
-    }
-    
     modifier onlyCitizen() {
         require(citizens[msg.sender].active == true, "Must have the citizen permission!");
         _;
@@ -25,10 +20,11 @@ contract TrashLife is Agents {
     }
     
     modifier payTARI() {
-        require(citizens[msg.sender].payTARI == true, "You have to pay the deposit before you can continue!");
+        require(citizens[msg.sender].payTARI == false, "You have to pay the deposit before you can continue!");
         _;
     }
     
+    event PayedTari(address _citizen, uint _time);
     
     uint constant deposit_mq_less4 = 2 * 10 **15; //2.mul(10**15);
     uint constant deposit_mq_more4 = 4 * 10 **15; //4.mul(10**15);
@@ -52,10 +48,11 @@ contract TrashLife is Agents {
     
     function payTari() external payable onlyCitizen {
         require(citizens[msg.sender].payTARI == false, "You have alredy paied the TARI!");
-        // Serve il revert se il require da errore?
-       
+
         if(msg.value == citizens[msg.sender].TARI) {
-            citizens[msg.sender].payTARI = true;} else {
+            citizens[msg.sender].payTARI = true;
+            emit PayedTari(msg.sender, now);
+        } else {
                 revert("The amount you are sending doesn't correspond to the TARI you have to pay!");
             }
     }
@@ -97,4 +94,47 @@ contract TrashLife is Agents {
        emit Received(msg.sender, _truck);
     }
     
+    // PAYOUT
+    event PayedPayout (address _address, uint _value, uint _time);
+    
+    function computePayout(address payable _citizen) private view returns(uint) {
+        uint totalW = citizens[_citizen].totalRecyclableWaste.add(citizens[_citizen].totalNonRecyclableWaste);
+        uint percentageRecycle = citizens[_citizen].totalRecyclableWaste.div(totalW)*100;
+        
+        if (percentageRecycle <= 25) {
+            return 0; 
+        } 
+        if (percentageRecycle <= 50 && percentageRecycle > 25) {
+            return citizens[_citizen].TARI.mul(2).div(100);
+        }
+        if (percentageRecycle > 50 && percentageRecycle <= 75) {
+            return citizens[_citizen].TARI.mul(5).div(100);
+        }
+        if (percentageRecycle > 75) {
+            return citizens[_citizen].TARI.mul(10).div(100);
+        }
+
+    }
+    
+    function withdraw() external onlyOwner {
+        uint balance = address(this).balance;
+        address payable to = msg.sender; 
+        (bool success, ) = to.call.value(balance)("");
+        require(success, "External transfer failed!");
+    }
+    
+    
+    function givePayout(address payable _citizen) external payable onlyOwner {
+        require(citizens[_citizen].active == true && citizens[_citizen].payTARI == true);
+        
+        // problema dei rientrance attacks 
+        citizens[_citizen].payTARI == false;
+        uint payout = computePayout(_citizen);
+        require(msg.sender.balance > payout, "Municipality has not enough funds!");
+        
+        (bool success, ) = _citizen.call.value(payout)("");
+        require(success, "External transfer failed!");
+        emit PayedPayout (_citizen, payout, now);
+    }
+ 
 }
