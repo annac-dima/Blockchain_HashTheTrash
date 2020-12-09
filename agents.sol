c49a69482eb1c0d3e7f496f764b4224810bc92b2
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-09 17:42:27 +0100

    Update Agents Contract (Version 1.0)

--- agents.sol
+++ agents.sol
@@ -7,16 +7,14 @@ contract Agents is Ownable {
     
     using SafeMath for uint; 
     using SafeMath for int;
-    using SafeMath for uint256;
     
     struct Citizen {
-        address payable citizenAddress; 
         string name; 
         uint family; 
         uint house; 
         uint weight;
+        
         uint TARI; 
-    
         uint totalRecyclableWaste; 
         uint totalNonRecyclableWaste;
         bool payTARI;
@@ -33,25 +31,16 @@ contract Agents is Ownable {
     struct Station {
         uint station_number; 
         uint weight;
+        int latitude;
         int longitude; 
-        int latitude; 
         bool waste; 
         bool active; 
     }
     
-    struct Municipality {
-        uint municipality_number; 
-        bool active;
-    }
-    
-    mapping(address => Municipality) public municipalities;
     mapping(address => Truck) public trucks; 
     mapping(address => Citizen) public citizens;
     mapping(address => Station) public stations; 
-    
-    uint municipalityCounter; 
-    event MunicipalityBorn(address payable indexed _municipality);
-    
+
     uint citizenCounter; 
     event CitizenBorn(address payable indexed _citizen, bool payTARI);
     
@@ -66,15 +55,13 @@ contract Agents is Ownable {
     function numberT() public view returns (uint) {return truckCounter;}
     function numberC() public view returns (uint) {return citizenCounter;}
     function numberS() public view returns (uint) {return stationCounter;}
-    function numberM() public view returns (uint) {return municipalityCounter;}
-    
+
     function createCitizen(address payable _address, string memory _name, 
     uint _family, uint _house, uint _w) public onlyOwner {
         require(citizens[_address].active == false);
         citizenCounter++; 
         
         citizens[_address] = Citizen(
-            _address, 
             _name,
             _family,
             _house,
@@ -108,13 +95,11 @@ contract Agents is Ownable {
         truckCounter--;
     }
     
-    function createStation(address _address, bool _recycle, int _long, int _lat) public onlyOwner {
+    function createStation(address _address, bool _recycle, int _lat, int _long) public onlyOwner {
         require(stations[_address].active == false);
         stationCounter++;
         stationNumber++;
-        //int long = _long.div(10);
-        //int lat = _lat.div(10);
-        stations[_address] = Station(stationNumber, 0, _long, _lat, _recycle, true);
+        stations[_address] = Station(stationNumber, 0, _lat, _long, _recycle, true);
         emit StationBorn(_address, _long, _lat);
     }
     
@@ -122,19 +107,6 @@ contract Agents is Ownable {
         require(stations[_address].active == true);
         delete stations[_address];
         stationCounter--;
-    }
-    
-    function createMunicipality(address payable _address) public onlyOwner {
-        require(municipalities[_address].active == false);
-        municipalityCounter++;
-        municipalities[_address] = Municipality(municipalityCounter, true);
-        emit MunicipalityBorn(_address);
-    }
-    
-    function deleteMunicipality(address _address) public onlyOwner {
-        require(municipalities[_address].active == true);
-        delete municipalities[_address];
-        municipalityCounter--;
     }
     
 }
