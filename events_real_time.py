081d354f3ea4c00cc7ad24e657d9ec052b8bc5c7
Davide Castellini <davide.castellini@studbocconi.it>
2020-12-11 17:49:07 +0100

    check events in real time

--- events_real_time.py
+++ events_real_time.py
@@ -1,63 +1,87 @@
 import json
-import random
 import pandas as pd
 import time
 from web3 import Web3
-from abi_bytecode import abi, bytecode # saved externally as .py
+from abi_bytecode import abi # saved externally as .py
 
 # Connecting to ganache through opened up PORT
 ganache_url = 'HTTP://127.0.0.1:7545'      #change here if different
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 web3.isConnected()
     
-deployed_ctr = web3.eth.contract(address=tx_receipt.contractAddress, abi=_abi) # contract
+
+# THINGS WE NEED!!  
+df = pd.read_csv('events.csv')
+abi = json.loads(abi)
 
 
-def get_past_logs(filter_list, poll_interval):
+def connect_to_contract():
     """
-    Iterates over every filter built and extracts logs from the block specified in the filter to the 'latest'
-    
-    inputs
-    ---------------
-     filter_list : filters created
-    
-    returns
-    ---------------
-     list containing every event attribute generated from the 'emit' on the contract
+    Connect to the contract to retrieve event logs
     """
-    events = []
+
+    with open('data/ctr_addr.txt', 'r') as f:
+        address = f.read()
+    print(f'Connected to ctr : {​​​​address}​​​​')
+
+    ctr = web3.eth.contract(address = address, abi = abi)
+    return ctr
+
+
+def create_filters(contract):
+    """
+    Create the filters to look for the selected events
+    """
+
+    f1 = contract.events.PickedUp.createFilter(fromBlock = 'latest')
+    f2 = contract.events.Deposited.createFilter(fromBlock = 'latest')
+    f3 = contract.events.Received.createFilter(fromBlock = 'latest')
+
+    return [f1, f2, f3]
+
+
+def handle_dataframe(df, event):
+    """
+    Update and save event logs in an external file
+    """
+
+    if not isinstance(event, dict):
+        raise Exception('The event must be passed in dictionary form')
+    df = df.append(event, ignore_index=True)
+    df.to_csv('events.csv', index=False)
+
+
+def handle_event(e):
+    """
+    Get the event logs from the latest block
+    """
+
+    args = dict(e['args'])
+    args.update(dict(e))
+    del args['args']
+
+
+def log_loop(df, filter_list, poll_interval):
+    """
+    Fetch events in real time
+    """
+
     while True:
         for event_filter in filter_list:
-            for e in event_filter.get_new_entries(): # get_new_entry() to check only last block
-                
-                # e is a nested dictionary, like this we bring all elements on the same level
-                args = dict(e['args'])
-                args.update(dict(e))
-                del args['args'] # brought one level above, delete the nest
-                # args.pop('args', None) # could delete like this too
-                
-                events.append(args)
-        time.sleep(poll_interval)    
-    return events
+            for e in event_filter.get_new_entries():
+                ev = handle_event(e)
+                handle_dataframe(df, ev)
 
-# Compiled abi and bytecode of trash.sol which inherits from citizenz.sol (and Ownable, safemath etc)
-abiRemix = json.loads(abi)         # turned to string after copy to clipboard from Remix
-bytecodeRemix = bytecode['object'] # it is a dictionary (as copy to clipboard form remix), we use the object for web3 deployment
-
-contract = deploy_contract(deployer=web3.eth.accounts[0], _abi=abiRemix, _bytecode=bytecodeRemix)
-
-pickedUp_filter = [contract.events.PickedUp.createFilter(fromBlock='latest')]
-
-def log_loop(event_filter, poll_interval):
-    while True:
-        for event in event_filter.get_new_entries():
-            handle_event(event)
         time.sleep(poll_interval)
 
 def main():
-    block_filter = w3.eth.filter('latest')
-    log_loop(block_filter, 2)
+    contract = connect_to_contract()
+    filter_list = create_filters(contract=contract)
+    log_loop(df=df, filter_list=filter_list, poll_interval=2)
 
+
+if __name__ == '__main__':
+    main()
 
 
 
