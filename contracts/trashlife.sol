99847514da374601812749d42c3e1047ec3e4453
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-12 20:17:07 +0100

    Update comments

--- contracts/trashlife.sol
+++ contracts/trashlife.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.6.0; 
 
+// Import Agents contract, used by the Municipality to create structs of citizens, trucks and disposal stations
 import "agents.sol";
 
 contract TrashLife is Agents {
@@ -22,42 +23,35 @@ contract TrashLife is Agents {
         _;
     }
     
-    
-    // Set the variable _withdraw equal to false to ensure that the municipality can call the "withdraw" function only once during the year
+    // The Municipality can withdraw ETH from the balance of this contract only once per year. To avoid double withdraws, the 
+    // following variable is set to false until the withdraw takes place. 
     bool _withdraw = false;
     
     
-    // Define an event to signal that a citizen has paid the TARI
+    
+    /////////////////////////  TARI PAYMENT  /////////////////////////
+    
+    // Define an event to signal that a citizen has paid the TARI and when this happened
     event PayedTari(address _citizen, uint _time);
     
-    /* Assign some constant values to the variables deposit_mq_less4, deposit_mq_more4 and deposit_trash, that are going to be needed for 
-    the computation of the TARI. In particular, the TARI is here given by the sum of two parts:
-        - the first part is given by the product of the square meters of the house and a fixed fee which depends on the number of people in 
-          the  household. If there are less than four people in the household, the fee deposit_mq_less4 applies, whereas if the household
-          has more than four members, the fee deposit_mq_more4 applies.
-        - the second part is variable and depends on the total amount of waste produced by the household the previous year. To be more 
-          specific, the total weight of waste produced by the household the year before is multiplied by a constant amount of money, i.e.
-          deposit_trash.
-    */
-    uint constant deposit_mq_less4 = 2 * 10 **15; //2.mul(10**15); // 1 euro
-    uint constant deposit_mq_more4 = 4 * 10 **15; //4.mul(10**15); // 2 euro
-    uint constant deposit_trash = 1 * 10 **14; // 5 cents
+    /* Define fixed fees to compute the TARI amount each citizen must pay; the TARI amount is defined as the sum of two parts:
+        1. Square meters of the house times a fee depending on the number of household members: If there are 4 or less people in the household, 
+        the deposit_mq_less4 fee applies, it corresponds to about 1 €/mq. If there are more than 4 people, deposit_mq_more4 applies, that is about 2€/mq.
+        2. Total waste produced by the household the previous year times a constant fee: Waste is measured in kg. The fee, deposit_trash, doesn't depend on 
+        anything and it's about 5 cents/kg. */
+    uint constant deposit_mq_less4 = 2 * 10 **15; 
+    uint constant deposit_mq_more4 = 4 * 10 **15;
+    uint constant deposit_trash = 1 * 10 **14;
     
-
-
-    /////////////////////////  TARI  /////////////////////////
+    // Define a function, only for the Municipaliy, to check the balance of the contract at a given point in time
+    function MunicipalityBalance() public view onlyOwner returns(uint) {return address(this).balance;}
     
-    // Define a function to check the balance of the contract at a given point in time
-    function MunicipalityBalance() external view onlyOwner returns(uint) {return address(this).balance;}
-    
-    /* Define a function to compute how much TARI a given citizen has to pay. This function is called by the municipality, and the citizen 
-    is notified about the amount of TARI he/she has to pay through a text message or an app notification. In this way, citizens do not have 
-    to bear the burden of calling this function, and their privacy is also protected. Indeed, other citizens cannot directly access the specific 
-    information about the amount of TARI that they have to pay.*/
+    // Define a function to compute how much TARI a given citizen has to pay. It is invoked at the beginning of the year by the Municipality, 
+    // that is in charge of informing each citizen 
     function TariAmount(address _address) public onlyOwner {
-        // Check that _address is associted with an existing citizen 
+        // Verify _address is associted with an existing citizen 
         require(citizens[_address].active == true, "Address is not a citizen!");
-        // Verify that the citizen has not paid the TARI yet. This is to avoid that the citizen pays the TARI twice in an year
+        // Verify the citizen has not paid the TARI yet. This is to avoid that the citizen pays twice in a year
         require(citizens[_address].payTARI == false, "You have alredy paid the TARI!");
         
         uint TARI = 0; 
@@ -66,28 +60,28 @@ contract TrashLife is Agents {
         } else {
             TARI = deposit_mq_more4 * citizens[_address].house + deposit_trash * citizens[_address].weight;
         }
+        // Update the TARI variable in the struct of each citizen
         citizens[_address].TARI = TARI;
     }
     
-    /* Define a function that can only be called by a citizen and allows him/her to pay the TARI to the municipality. To be more specific, 
-    the citizen sends the money to this contract, which is owned by the municipality */
+    // Define a function, only for citizens, to pay the exact amount of TARI. Amounts are expressend in wei and stored in this contract. 
     function payTari() external payable onlyCitizen {
-        // Verify that the citizen has not paid the TARI yet. This is to avoid that the citizen pays the TARI twice in an year 
+        // Verify the citizen has not paid the TARI yet. This is to avoid that the citizen pays twice in a year 
         require(citizens[msg.sender].payTARI == false, "You have alredy paied the TARI!");
 
-        // This is to check whether the citizen is paying to the municipality the correct amount of money. If the amount that the citizen
-        // is sending does not correspond to the TARI he/she has to pay, the payment is reverted
+        // Verify the citizen is paying the correct amount of money
         if(msg.value == citizens[msg.sender].TARI) {
             citizens[msg.sender].payTARI = true;
             citizens[msg.sender].weight = 0; 
             emit PayedTari(msg.sender, now);
         } else {
+                // If the citizen pays a different wei amount, the payment is reverted
                 revert("The amount you are sending doesn't correspond to the TARI you have to pay!");
             }
     }
     
     
-
+    
     /////////////////////////  TRASH CYCLE /////////////////////////
 
     // Define the events that are going to keep track of the life cycle of each trash bag
@@ -100,38 +94,38 @@ contract TrashLife is Agents {
         return keccak256(abi.encodePacked(_citizen, _pickUpTime, _random));
     }
     
-    /* Define a function for the pick up of trash bags. Only trucks can call this function. 
-    As regards the arguments of the function, the address _citizen comes directly from some sensors placed in each truck, which scan and
-    store the information printed on each trash bag. Each truck is also equipped with a scale, so the uint _wasteWeight also comes 
-    directly from the external world. The uint random is simply a random number which is needed for computing the unique id of each 
-    trash bag.
-    */
+    /* Define a function to regsiter the pick up of trash bags. Only truck drivers can call this function, specifying:
+        - the address of the citizen the bag belongs to: printed on the bag and scanned by the truck driver;
+        - the weight of the bag: provided by a scale included in the truck;
+        - a random integer to ensure the hash-ID of the bag is really unique.*/
     function pick(address _citizen, uint _wasteWeight, uint _random) external onlyTruck() {
         bytes32 uniqueBagId = _computeIdBag(_citizen, now, _random);
+        // Increase the weight attribute of the truck by the weight of each bag it picks up
         trucks[msg.sender].weight = trucks[msg.sender].weight.add(_wasteWeight);
         
         if(trucks[msg.sender].waste == false){
             citizens[_citizen].totalNonRecyclableWaste = citizens[_citizen].totalNonRecyclableWaste.add(_wasteWeight);
+            // Increase the weight of NON-recyclable waste the citizen has produced in this year 
         } else {citizens[_citizen].totalRecyclableWaste = citizens[_citizen].totalRecyclableWaste.add(_wasteWeight);}
+            // Increase the weight of recyclable waste the citizen has produced in this year 
      
         emit PickedUp(msg.sender, trucks[msg.sender].waste, uniqueBagId, _citizen, _wasteWeight, now);
     }
     
-    /* Define a function for the dumping of trash bags at the appropriate disposal station. The function can only be called by trucks, who
-    have to specify the address of the station as the input of the function. The two integers _latitudeTruck and _longitudeTruck represent
-    instead the GPS coordinates of the trucks, and come directly from the external world. */
+    /* Define a function to register the dumping of trash bags at the appropriate disposal station. Only truck drivers can call this function, specifying:
+        - the address of the station they are at;
+        - the longitude and latitude of their position: provided directly by their GPS coordinates. */
     function drop(address _disposalStation, int _latitudeTruck, int _longitudeTruck) external onlyTruck() {
-        // Check whether the GPS coordinates of the truck correspond to the location of a disposal station which the municipality
-        // has previously approved and included in the mapping stations
+        // Verify the GPS coordinates of the truck correspond to the location of an approved disposal station
         require(stations[_disposalStation].latitude == _latitudeTruck && stations[_disposalStation].longitude == _longitudeTruck, 
             "You are at the wrong station!");
-        // Check whether the truck is at the right station on the basis of the type of waste it is carrying (recyclable or non-recyclable)
+        // Verify the truck is at the right station on the basis of the type of waste it is carrying (recyclable or non-recyclable)
         require(trucks[msg.sender].waste == stations[_disposalStation].waste, "You are at the wrong station!");
         
         emit Deposited(msg.sender, trucks[msg.sender].waste, trucks[msg.sender].weight, _disposalStation);
-        // Increase the weight counter of the station with the total weight received by the truck
+        // Increase the weight attribute of the station by the total weight of waste dropped by the truck
         stations[_disposalStation].weight = stations[_disposalStation].weight.add(trucks[msg.sender].weight);
-        // Set the "weight" attribute of the truck equal to zero once the truck has dropped its content to the station
+        // Reset the weight attribute of the truck since it's now empty 
         trucks[msg.sender].weight = 0;
 
     }
@@ -142,8 +136,8 @@ contract TrashLife is Agents {
     specify the address of the truck in question, the type of waste that the station disposes and the total weight of trash that has been
     dumped to the station so far.*/
     function received(bool _waste, address _truck, uint _weight) external onlyStation() {
-        // Check whether the type of waste that the truck is carrying (recyclable or not) is coherent with the type of waste that the 
-        // station disposes, and whether the total amount of waste at the station (previous amount + weight of trash dumped by the 
+        // Verify the coherence between the waste type (recyclable or not) of the truck and the station
+        // Verify the total amount of waste at the station (previous amount + weight of trash dumped by the 
         // truck in question) is equal to the amount of waste that the station declares to have received up to now (i.e. _weight).
         require(trucks[_truck].waste == _waste && stations[msg.sender].weight == _weight);
         emit Received(msg.sender, _truck);
@@ -153,50 +147,41 @@ contract TrashLife is Agents {
     
     /////////////////////////  PAYOUTS  /////////////////////////
     
-    // Define an event to signal that the municipality has paid the respective payout to a citizen 
+    // Define an event to signal that the municipality has paid the respective payout to a citizen, including payment amount and time
     event PayedPayout (address _address, uint _value, uint _time);
     
-    /* Define a function to compute the payout that a citizen has earned during the year. In particular, the payout depends on the
-    fraction of recyclable waste produced by the citizen during the year, and is computed as a percentage of the TARI paid
-    by the citizen at the beginning of the year */
+    // Define a function to compute, at the end of the year, the payout a citizen is entitled to
     function _computePayout(address payable _citizen) private view returns(uint) {
         // Get the total amount of waste a citizen has generated during the current year
         uint totalW = citizens[_citizen].totalRecyclableWaste.add(citizens[_citizen].totalNonRecyclableWaste);
         // Compute the percentage of recyclable waste
         uint percentageRecycle = citizens[_citizen].totalRecyclableWaste.mul(100).div(totalW);
         
-        // If the percentage of recyclable waste is below 25% the citizen is not eligible for a reimburse 
+        // If this percentage is below 25% the citizen is not eligible for a reimburse 
         if (percentageRecycle <= 25) {
             return 0; 
         } 
-        // If the percentage of recyclable waste is between 25% and 50% the citizen receives back 2% of the TARI paid
-        // at the beginning of the year
+        // If this percentage is between 25% and 50% the citizen receives back 2% of the TARI paid at the beginning of the year
         if (percentageRecycle <= 50 && percentageRecycle > 25) {
             return citizens[_citizen].TARI.mul(2).div(100);
         }
-        // If the percentage of recyclable waste is between 50% and 75% the citizen receives back 5% of the TARI paid
-        // at the beginning of the year
+        // If this percentage is between 50% and 75% the citizen receives back 5% of the TARI paid at the beginning of the year
         if (percentageRecycle > 50 && percentageRecycle <= 75) {
             return citizens[_citizen].TARI.mul(5).div(100);
         }
-        // If the percentage of recyclable waste is above 75% the citizen receives back 5% of the TARI paid at the beginning of the year
+        // If this percentage is above 75% the citizen receives back 10% of the TARI paid at the beginning of the year
         if (percentageRecycle > 75) {
             return citizens[_citizen].TARI.mul(10).div(100);
         }
         
     }
     
-    /* Define a function that allows the municipality to withdraw some funds from the contract before the end of the year. In particular, 
-    if we supposed that all citizens recycled more than 75% of their waste, the maximum amount that the municipality would have to pay is 
-    10% of the total TARI paid by all citizens (i.e. 10% of the money stored in this contract). Therefore, the municipality can safely
-    withdraw 88% of the funds stored in this contract and thus enjoy immediate liquidity, without compromising the ability of reimbursing 
-    all the citizens at the end of the year. An additional 2% of the funds is kept in the contract to also take into account eventual 
-    transaction costs (this is why the municipality can only withdraw 88% of the money stored in the contract and not 90%).*/
+    // Define a function that allows the Municipality to withdraw some funds from the contract before the end of the year
     function withdraw() public onlyOwner returns(uint){
-        // For simplicity, we assume that the municipality can call this function only once every year
+        // For simplicity the municipality can call this function only once every year
         require (_withdraw == false);
-        // The variable _withdraw is set equal to true to avoid potential re-entrancy attacks
         _withdraw = true;
+        // The municipality can withdraw 88% of the funds stored in this contract, since they won't be necessary for any payout
         uint balance = (address(this).balance).mul(88).div(100);
         address payable to = msg.sender; 
         (bool success, ) = to.call{value:balance}("");
@@ -204,14 +189,10 @@ contract TrashLife is Agents {
         return balance;
     }
 
-    /*Function to define whether it is the appropriate time to call the function "givePayout". The municipality can indeed
-    call the "givePayout" function only at the end of each year. In particular, the municipality is allowed to call this function only
-    between 20th and 28th of december. This time range was chosen in order to to avoid the potential complications that could arise if, 
-    for some reasons, the municipality did not call the "setBeginningYear" function exactly on 1st January, or if the miners took days to 
-    mine the block with the "setBeginningYear" function. */
+    // Define whether it is the appropriate time to call "givePayout", since payouts must be payed at the end of the year 
     function _isAppropriateTime() private view onlyOwner returns(bool) {
-        uint date1 = start + 353 days;
-        uint date2 = start + 361 days;
+        uint date1 = start + 353 days; // From 20th December 
+        uint date2 = start + 361 days; // To 28th December
         bool appropriate;
         if (now >= date1 && now <= date2) {
             appropriate = true;
@@ -219,7 +200,7 @@ contract TrashLife is Agents {
         return appropriate; 
     }
     
-    // Define a function so that the municipality can pay the payouts earned by the citizens 
+    // Define a function to allow the Municipality paying the payouts 
     function givePayout(address payable _citizen) external payable onlyOwner {
         // Check that the address _citizen is associted with an existing citizen, and that the citizen in question has already paid the TARI
         require(citizens[_citizen].active == true && citizens[_citizen].payTARI == true);
@@ -251,13 +232,12 @@ contract TrashLife is Agents {
         return appropriate;
     }
 
-    /* Define a function that allows the municipality to destroy the contract at the end of each year. The money that are still
-    stored in the contract when this function is called are sent to the Ethereum address of the municipality. */
+    /* Define a function to destroy the contract at the end of each year. Only the Municipality can call it
     function destroyContract() public onlyOwner {
-        // Check whether it is the appropriate time for the municipality to call this function. This line of code is commented for the 
-        // purposes of the Python simulation. 
-        //require(_timeToDestroyContract() == true);
+        // Check whether it is the appropriate time for the municipality to call this function. 
+        //require(_timeToDestroyContract() == true); // commented for the purpose of Python test
         selfdestruct(msg.sender);
+        // The contract balance is automatically transferred to msg.sender, that is the Municipality
     }
     
 }
